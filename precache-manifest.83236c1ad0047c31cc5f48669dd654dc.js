180528684aa5887ecbf689bed3203a10e18622ab
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-11-26 17:11:29 -0700

    Updates

---
+++ precache-manifest.83236c1ad0047c31cc5f48669dd654dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39c71564f64654c85a2420fd738f325",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "415d45c544c5abcb9820",
+    "url": "/jamming/static/css/main.2730363e.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/jamming/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "415d45c544c5abcb9820",
+    "url": "/jamming/static/js/main.1a474f85.chunk.js"
+  },
+  {
+    "revision": "c3d7dfc925f613b07d70",
+    "url": "/jamming/static/js/runtime~main.6e9a7a58.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
